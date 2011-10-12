6652176dbfeac77ce313a681a96131debb07551e
Victor Velazquez <victor.velazquez@crowdint.com>
2011-10-12 18:38:32 -0500

    Added Class

--- fibo.rb
+++ fibo.rb
@@ -1,12 +1,14 @@
-def closest_fibonacci
-  a,b = 0,1
-  begin
-   self.times  do
-     a,b = b,a+b
-     if(b>=self)
-       puts "#{a}"
-       break
+class Fixnum
+  def closest_fibonacci
+    a,b = 0,1
+    begin
+     self.times  do
+       a,b = b,a+b
+       if(b>=self)
+         puts "#{a}"
+         break
+       end
      end
-   end
+    end
   end
 end

15d4fb9903cd67b403411a9bebfc3e34a1c5f48e
Victor Velazquez <victor.velazquez@crowdint.com>
2011-10-12 17:08:03 -0500

    closest fibonacci

--- fibo.rb
+++ fibo.rb
@@ -1,21 +1,12 @@
 def closest_fibonacci
-  printf "\nHow many numbers of the sequence would you like?\n"
-  n = STDIN.readline.to_i
-  fibonacci(n)
-end
-def fibonacci(n)
   a,b = 0,1
   begin
-   n.times  do
-       a,b = b,a+b
-       if(b>=n)
-         printf("el fibonaci anterior es=> %d\n",a)
-         break
-       end
+   self.times  do
+     a,b = b,a+b
+     if(b>=self)
+       puts "#{a}"
+       break
+     end
    end
   end
-
 end
-
-
-closest_fibonacci
